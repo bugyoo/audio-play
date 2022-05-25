524c776132dea0bb7e9c0284d7373f58662b6ad3
李树恒 <lishers@163.com>
2022-05-25 14:06:58 +0800

    优化 播放切换

--- component/audio-play/index.js
+++ component/audio-play/index.js
@@ -1,5 +1,4 @@
 
-const innerAudioContext = wx.createInnerAudioContext()
 Component({
   properties :{
     url:{
@@ -10,19 +9,23 @@ Component({
   data: {
     isplay:false,
     totalTime:0,
-    currentTime:0
+    currentTime:0,
+    innerAudioContext:null,
+    changing:false
   },
   ready() {
+    const innerAudioContext = wx.createInnerAudioContext()
     innerAudioContext.autoplay = false
     innerAudioContext.src = this.data.url
     // innerAudioContext.src = 'https://v.dyjyzyk.dtdjzx.gov.cn/resource-mydzb/2222.mp3'
     // innerAudioContext.src = 'https://v.dyjyzyk.dtdjzx.gov.cn/resource-mydzb/王佳杨-孤独.flac'
     innerAudioContext.onTimeUpdate((res)=>{
-      console.log(res,'======>');
-      this.setData({
-        totalTime:Math.ceil(innerAudioContext.duration),
-        currentTime:Math.ceil(innerAudioContext.currentTime)
-      })
+      if(!this.data.changing){
+        this.setData({
+          totalTime:Math.ceil(innerAudioContext.duration),
+          currentTime:Math.ceil(innerAudioContext.currentTime)
+        })
+      }
     })
     innerAudioContext.onCanplay(()=>{
       this.setData({
@@ -36,34 +39,50 @@ Component({
       })
     })
     innerAudioContext.onError((res) => {
+      console.log(res,'错误');
       this.setData({
         currentTime:0,
         isplay:false
       })
       wx.showToast({
-        title: res.errMsg,
+        title: '该格式音频暂不支持播放',
         icon:"none"
       })
     })
+
+    this.setData({innerAudioContext})
   },
   methods:{
+    bindchanging(){
+      this.setData({
+        changing:true
+      })
+    },
     slider1change(e){
       this.setData({
+        changing:false,
         currentTime:e.detail.value
       })
-      innerAudioContext.seek(e.detail.value)
+      this.data.innerAudioContext.seek(e.detail.value)
       this.playHanlder()
     },
     playHanlder(){
-      innerAudioContext.play()
+      this.data.innerAudioContext.play()
       this.setData({isplay:true})
     },
     pauseHanlder(){
-      innerAudioContext.pause()
+      this.data.innerAudioContext.pause()
       this.setData({isplay:false})
     },
   },
   detached(){
-    innerAudioContext.destroy()
+    if(this.data.innerAudioContext){
+      this.data.innerAudioContext.offTimeUpdate()
+      this.data.innerAudioContext.offCanplay()
+      this.data.innerAudioContext.offEnded()
+      this.data.innerAudioContext.offError()
+      this.data.innerAudioContext.destroy()
+      this.data.innerAudioContext = null
+    }
   },
 })
